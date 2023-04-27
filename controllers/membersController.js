78313df4c63b30a87bf55e86500c8b0920b594b8
Patrick <angelopatrickriosnolasco@gmail.com>
2023-04-26 19:37:07 -0500

    feat: error handling

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -1,7 +1,12 @@
 const { pool } = require("../config/db");
 const getAllMembers = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM users");
-  res.json(rows);
+  try {
+    const [rows] = await pool.query("SELECTs * FROM users");
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 const registerMember = async (req, res) => {
   const { name, lastName, dateRegister, phoneNumber, email, imageProfile } =
@@ -19,26 +24,31 @@ const registerMember = async (req, res) => {
     return res.status(400).json({ message: "All fields are required" });
   }
 
-  // Check for duplicate member
-  const [rows] = await pool.query(
-    "SELECT id FROM users WHERE name = ? OR phoneNumber = ? OR email= ?",
-    [name, phoneNumber, email]
-  );
-  if (rows.length > 0) {
-    return res.status(409).json({ message: "There is a duplicate field" });
-  }
+  try {
+    // Check for duplicate member
+    const [rows] = await pool.query(
+      "SELECT id FROM users WHERE name = ? OR phoneNumber = ? OR email= ?",
+      [name, phoneNumber, email]
+    );
+    if (rows.length > 0) {
+      return res.status(409).json({ message: "There is a duplicate field" });
+    }
 
-  // Create and store the new user
-  const [result] = await pool.query(
-    "INSERT INTO users (name, lastName, dateRegister, phoneNumber, email, imageProfile) VALUES (?, ?, ?, ?, ?, ?)",
-    [name, lastName, dateRegister, phoneNumber, email, imageProfile]
-  );
+    // Create and store the new user
+    const [result] = await pool.query(
+      "INSERT INTO users (name, lastName, dateRegister, phoneNumber, email, imageProfile) VALUES (?, ?, ?, ?, ?, ?)",
+      [name, lastName, dateRegister, phoneNumber, email, imageProfile]
+    );
 
-  if (result) {
-    // Created
-    return res.status(201).json({ message: "New member created" });
-  } else {
-    return res.status(400).json({ message: "Invalid member data received" });
+    if (result) {
+      // Created
+      return res.status(201).json({ message: "New member created" });
+    } else {
+      return res.status(400).json({ message: "Invalid member data received" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 module.exports = { registerMember, getAllMembers };
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+require("express-async-errors");
 const path = require("path");
 const app = express();
 const port = 3000;
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 
+app.use(logger);
 app.use(cors(corsOptions));
 app.use(express.json());
 // routes
@@ -25,3 +29,5 @@ app.all("*", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.use(errorHandler);
